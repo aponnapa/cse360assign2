29776b2a77093658dac7abf6db160c9af9dd65e7
aponnapa <47870361+aponnapa@users.noreply.github.com>
2019-02-21 15:00:09 -0700

    Second Commit

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -5,6 +5,7 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
+	private int totalstring;
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
@@ -12,22 +13,29 @@ public class Calculator {
 	
 	public int getTotal () {
 		return 0;
+		
 	}
 	
 	public void add (int value) {
-		
+		total=total+value;
 	}
 	
 	public void subtract (int value) {
-		
+		total=total-value;
 	}
 	
 	public void multiply (int value) {
-		
+		total=total*value;
 	}
 	
 	public void divide (int value) {
-		
+		if(value==0)
+		{
+			total=0;
+		}
+		else{
+		total=total/value;
+		}
 	}
 	
 	public String getHistory () {
