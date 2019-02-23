ba00dea7e77000ee44a9695791a75905cddf1d85
aponnapa <47870361+aponnapa@users.noreply.github.com>
2019-02-23 12:28:59 -0700

    First Commit

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,42 +1,65 @@
+/**
+ * Aditya Ponnapalli
+ * CSE 360 Wed 
+ * Class ID 525
+ * Assignment 2
+ * This file contain a calculator class which
+ * contains methods for adding, subtracting, 
+ * multiplying, and dividing int total and int value
+ * It also contains a method which will produce
+ * a history of all actions as a string 
+ */
 package cse360assign2;
 
-
-
+/**
+ * Creating the calculator class
+ */
 public class Calculator {
 
 	private int total;
 	
 	public Calculator () {
-		total = 0;  // not needed - included for clarity
+		total = 0;  
 	}
 	
 	public int getTotal () {
-		return total;
+		return 0;
+	}
+/**
+ * This method will add value to total
+ * @param value
+ */
+	public void add (int value) {
 		
 	}
 	
-	public void add (int value) {
-		total = total + value;
-	}
-	
+/**
+ * This method will subtract value from total
+ * @param value
+ */
 	public void subtract (int value) {
-		total = total - value;
+		
 	}
-	
+/**
+ * This method will multiply value and total	
+ * @param value
+ */
 	public void multiply (int value) {
-		total = total * value;
+		
 	}
-	
+/**
+ * This method will divide total by value
+ * @param value
+ */
 	public void divide (int value) {
-		if(value == 0)
-		{
-			total = 0;
-		}
-		else{
-		total = total / value;
-		}
+		
 	}
-	
+
+/**
+ * This method will return the history of all
+ * actions as a string
+ * @return
+ */
 	public String getHistory () {
 		return "";
 	}
